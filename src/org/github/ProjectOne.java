a5fdb65c7384b414867b0fcc8530b219ad6e0978
navgam <navgame92@gmail.com>
2021-12-07 11:44:49 +0530

    Password functionality completed

--- src/org/github/ProjectOne.java
+++ src/org/github/ProjectOne.java
@@ -1,5 +1,12 @@
 package org.github;
 
 public class ProjectOne {
-
+     
+	public static void password() {
+     System.out.println("Password functionality");
+	}
+	
+	public static void main(String[] args) {
+		password();
+	}
 }
