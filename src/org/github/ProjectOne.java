87d753ef440c5dbdb5ad35dba1c0e3d5ccf9e29b
navgam <navgame92@gmail.com>
2021-12-07 11:37:07 +0530

    userName Functionality completed

--- src/org/github/ProjectOne.java
+++ src/org/github/ProjectOne.java
@@ -2,4 +2,11 @@ package org.github;
 
 public class ProjectOne {
 
+	public static void sample() {
+		System.out.println("Login functionality");
+}
+	
+	public static void main(String[] args) {
+		sample();
+	}
 }
